9c993c9c4d1c03968d9026d79b1987d5cfccc2d6
unknown <csun11@.otsads.iit.edu>
2010-12-05 22:59:06 -0600

    removed pubmgmt file; added more js for resume and portfolio_index pages

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 $(document).ready(function() { 
+	$('h3').addClass('hasjs');
+	$('h3').click(function() {
+		  $(this).siblings('*').toggle();
+		  });
 	$('ul#portfolio_index li').addClass('hasjs');
 	$('ul#portfolio_index p').click(function(){
 			 $(this).siblings('.pf_description').animate({
@@ -7,7 +11,13 @@ $(document).ready(function() {
 				}, 
 				100, 'linear');
 		});
-	$('ul#portfolio_index p').click(function(){
-			$('p').fadeToggle("slow","linear");
-		});
+
+	$("ul#portfolio_index .pf_quote").hover(function(){
+        
+        $(this).stop().animate({marginLeft : "20px"},100,"swing");},
+			function(){
+				$(this).stop().animate({marginLeft : "0px"},600,"swing");}
+        
+    );
+
 });
