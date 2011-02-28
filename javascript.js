8a68e47f5149636c7ee655bc6b0de4c0ca08a97d
unknown <csun11@.otsads.iit.edu>
2011-02-28 11:23:40 -0600

    made the vertical nav bar in resume page floating; added the final usability report to usability portfolio page. To do: 1) update the content of resume; 2) update the portfolio pages; 3) the jQuery for click-toggling on portfolio index page does not work, consider redesigning.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
+
+    var menuYloc = null;
+	var name = "#vertical_bar";
+
 $(document).ready(function() { 
-	$('h3').siblings('*').slideUp(300).delay(900).fadeIn(1200);
-	$('h3').addClass('hasjs');
-	$('h3').click(function() {
-		  $(this).siblings('*').toggle();
-		  });
+
+/* trying to create click-toggling effects on portfolio page; however it does not work well now.
 	$('ul#portfolio_index li').addClass('hasjs');
 	$('ul#portfolio_index p').click(function(){
 			 $(this).siblings('.pf_description').animate({
@@ -11,14 +12,33 @@ $(document).ready(function() {
 				height: ['toggle', 'swing'],
 				}, 
 				100, 'linear');
-		});
+		});  
+*/
 
+
+//adding nudging effects for the "See my work samples" link.
 	$("ul#portfolio_index .pf_quote").hover(function(){
-        
         $(this).stop().animate({marginLeft : "20px"},100,"swing");},
 			function(){
-				$(this).stop().animate({marginLeft : "0px"},600,"swing");}
-        
-    );
+				$(this).stop().animate({marginLeft : "0px"},600,"swing");
+	});
+
+
+
+//adding sliding and click-toggling effects on resume page
+	$('h3').siblings('*').slideUp(300).delay(900).fadeIn(1200);
+	$('h3').addClass('hasjs');
+	$('h3').click(function() {
+		  $(this).siblings('*').toggle();
+		  });
+
+
+//make the vertical navigation bar to be always visible floating on right
+	    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))
+        $(window).scroll(function () {
+            var offset = menuYloc+$(document).scrollTop()+"px";
+            $(name).animate({top:offset},{duration:500,queue:false});
+        });
+
 
 });
